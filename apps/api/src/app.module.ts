fef50dbde0e293dcc1875ba37d9d02b9fbc8153c
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-03-16 00:31:31 +0000

    feat: add core modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProfilesModule } from './profiles/profiles.module';
+import { SearchModule } from './search/search.module';
+import { JobModule } from './job/job.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [ProfilesModule],
+  imports: [ProfilesModule, SearchModule, JobModule, AdminModule],
 })
 export class AppModule {}
