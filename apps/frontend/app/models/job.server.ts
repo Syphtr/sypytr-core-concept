e98f6cf72f30f13bff51c20b48195a9b5eb5729d
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-03-20 01:40:04 +0000

    build: remove prisma from frontend

--- apps/frontend/app/models/job.server.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from "~/db.server";
-
-export async function getJobs() {
-  return await prisma.job.findMany({
-    take: 15,
-  });
-}
--- apps/frontend/app/routes/dashboard._index.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function DashBoardIndexRoute() {
-  return (
-    <div>
-      <h1>Overview</h1>
-    </div>
-  );
-}
